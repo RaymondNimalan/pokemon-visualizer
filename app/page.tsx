d4e365875fc88ed06d72cca07f5c75b6c2a44cfe
Ray Nimalan <raymondnimalan@gmail.com>
2024-09-12 16:46:27 -0400

    feat: add fetch request

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
+const getPokemon = async () => {
+    const data = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+    return data.json();
+};
+
+const Home = async () => {
+    const pokemon = await getPokemon();
+    console.log(pokemon);
     return <div></div>;
-}
+};
+
+export default Home;
