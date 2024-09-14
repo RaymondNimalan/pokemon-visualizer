3a2140dd7378cea62ab8b70358cc8220c00f89ba
Ray Nimalan <raymondnimalan@gmail.com>
2024-09-14 14:41:30 -0400

    feat: add bar chart component

---
+++ components/BarChart.tsx
@@ -0,0 +1,63 @@
+'use client';
+import {
+    BarElement,
+    CategoryScale,
+    Chart as ChartJS,
+    Legend,
+    LinearScale,
+    Title,
+    Tooltip,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+interface BarData {
+    barData: {
+        fire: number;
+        water: number;
+        grass: number;
+    };
+}
+const BarChart = ({ barData }: BarData) => {
+    console.log(barData);
+
+    const Data = {
+        labels: ['1', '2', '3'],
+        datasets: [
+            {
+                label: 'Pokemon Types',
+                data: [1, 2, 3],
+                borderColor: 'blue',
+                borderWidth: 2,
+            },
+        ],
+    };
+    return (
+        <div>
+            <Bar
+                data={Data}
+                options={{
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Users Gained between 2016-2020',
+                        },
+                        legend: {
+                            display: false,
+                        },
+                    },
+                }}
+            />
+        </div>
+    );
+};
+
+export default BarChart;
