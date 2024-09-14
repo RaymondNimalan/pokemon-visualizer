ea2a02b0148ec54c68a8e8b478e61a0b30df36c3
Ray Nimalan <raymondnimalan@gmail.com>
2024-09-14 17:18:51 -0400

    feat: add func to fetch all urls and count types

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,48 @@ const getPokemon = async () => {
     const data = await response.json();
     return data.results;
 };
+
+const getPokeData = async (data) => {
+    const pokePromises = data.map(
+        async (pokemon: { name: string; url: string }) => {
+            const response = await fetch(pokemon.url);
+            return await response.json();
+        }
+    );
+    const pokeData = await Promise.all(pokePromises);
+
+    const typesData = pokeData.reduce(
+        (
+            typesCount: {
+                typesSum: { [key: string]: number };
+                singleType: number;
+                doubleType: number;
+            },
+            pokemon
+        ) => {
+            if (pokemon.types.length === 1) {
+                typesCount.singleType += 1;
+            } else if (pokemon.types.length === 2) {
+                typesCount.doubleType += 1;
+            }
+            pokemon.types.forEach((typeInfo: { type: { name: string } }) => {
+                const typeName = typeInfo.type.name;
+                typesCount.typesSum[typeName] =
+                    (typesCount.typesSum[typeName] || 0) + 1;
+            });
+            return typesCount;
+        },
+        { typesSum: {}, singleType: 0, doubleType: 0 }
+    );
+
+    return typesData;
 };
 
 const Home = async () => {
     const pokemon = await getPokemon();
-    console.log(pokemon);
-    return <div></div>;
+    const pokeData = await getPokeData(pokemon);
+    return (
+    );
 };
 
 export default Home;
