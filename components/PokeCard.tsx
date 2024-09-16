341b932ea256739ec5ae7a083a376a8b41eccd97
Ray Nimalan <raymondnimalan@gmail.com>
2024-09-15 20:13:51 -0400

    feat: add new styles to card

--- components/PokeCard.tsx
+++ components/PokeCard.tsx
@@ -3,18 +3,19 @@ interface PokeCardProps {
 }
 const PokeCard = (pokeData: PokeCardProps) => {
     const data = pokeData.pokeData;
-    console.log('inside card', data);
-    console.log(data.sprites.other.dream_world.front_default);
+    // console.log('inside card', data);
+    // console.log(data.sprites.other.dream_world.front_default);
     const imageUrl = data.sprites.other.dream_world.front_default;
     const name = data.name;
     return (
-        <div className='bg-white w-[150px] h-[220px] rounded-md m-6 items-center flex flex-col'>
-            <div className='flex w-[190px] justify-center items-center'>
-                <img src={imageUrl} />
-            </div>
-
-            <div>
-                <p className='text-black'>{name}</p>
+        <div className='bg-white w-[176px] h-[220px] rounded-lg items-center flex flex-col'>
+            <div className='size-full rounded-lg items-center flex flex-col border-4 border-black'>
+                <div className='flex h-[175px] justify-center items-center'>
+                    <img src={imageUrl} className='h-[125px] p-2' />
+                </div>
+                <div className='flex bg-red-500 rounded-t-full rounded-b-lg w-[169px] h-full justify-center items-center border-2 border-black border-b-0'>
+                    <p className='text-black'>{name}</p>
+                </div>
             </div>
         </div>
     );
