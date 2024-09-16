308cc4a48bc4a814b2345e074295da17caebe536
Ray Nimalan <raymondnimalan@gmail.com>
2024-09-15 20:17:33 -0400

    feat: add local state and hook to save data

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useEffect, useState } from 'react';
 
 interface PokeCardProps {
     pokeData: [];
@@ -6,6 +7,15 @@ interface PokeCardProps {
 
 interface Pokemon {}
 const SearchBar = ({ pokeData }: PokeCardProps) => {
+    const [searchedPokemon, setSearchedPokemon] = useState('');
+    const [error, setError] = useState(false);
+    const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+    const [filteredList, setFilteredList] = useState<Pokemon[]>([]);
+    console.log(pokeData);
+
+    useEffect(() => {
+        setPokemonList(pokeData);
+    }, []);
     return (
         <div className='w-full flex flex-col w-[600px] items-center'>
         </div>
