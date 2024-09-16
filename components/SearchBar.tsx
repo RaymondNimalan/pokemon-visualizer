21176284c1977cf98906a889c05201f78da2c966
Ray Nimalan <raymondnimalan@gmail.com>
2024-09-15 20:21:26 -0400

    feat: add input and filtered cards grid

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,8 @@
 'use client';
+import { Input } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import PokeCard from './PokeCard';
 
 interface PokeCardProps {
     pokeData: [];
@@ -25,6 +28,42 @@ const SearchBar = ({ pokeData }: PokeCardProps) => {
 
     return (
         <div className='w-full flex flex-col w-[600px] items-center'>
+            <div className='flex flex-col gap-4 w-fullitems-center'>
+                <div className='flex p-4 w-[400px]'>
+                    <Input
+                        type='text'
+                        value={searchedPokemon}
+                        // onChange={(e) => setSearchedPokemon(e.target.value)}
+                        onChange={handleInputChange}
+                        // isClearable
+                        radius='lg'
+                        classNames={{
+                            innerWrapper: [
+                                'flex',
+                                'flex-row',
+                                'gap-4',
+                                'w-full',
+                                'justity-center',
+                                'items-center',
+                                'text-[24px]',
+                            ],
+                            input: ['h-[32px]'],
+                        }}
+                        placeholder='Type to search...'
+                        startContent={<FaSearch />}
+                    />
+                </div>
+            </div>
+            <div className='w-full flex-wrap flex p-4 justify-evenly'>
+                {filteredList.map((pokemon) => (
+                    <PokeCard key={pokemon.id} pokeData={pokemon} />
+                ))}
+            </div>
+            {/* <div>
+                {pokeData.map((pokemon) => (
+                    <PokeCard key={pokeData.id} pokeData={pokemon} />
+                ))}
+            </div> */}
         </div>
     );
 };
