4a0bfeb85fe491f3c189ce4a0b5060d7c03d0d2c
Ray Nimalan <raymondnimalan@gmail.com>
2024-09-15 23:44:25 -0400

    feat: add components to dashboard with styling

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,6 +1,36 @@
 'use client';
-const Dashboard = () => {
-    return <div></div>;
+import BarChart from './BarChart';
+import PieChart from './PieChart';
+import PokeStats from './PokeStats';
+import SearchBar from './SearchBar';
+
+interface PokeCardProps {
+    pokeData: object;
+}
+const Dashboard = ({ pokeData }: PokeCardProps) => {
+    return (
+        <div className='flex size-full gap-6 p-8 lg:flex-row flex-col overflow-visible justify-center items-center'>
+            <div className='flex-col justify-center items-center'>
+                <div className='flex-col h-full p-4 max-w-[400px] gap-4 bg-blue-600 rounded-lg'>
+                    <h2>Pokemon Types Stats</h2>
+                    <div className='flex-1 bg-white rounded-lg w-[150px]'>
+                        <PieChart
+                            pieData={[
+                                pokeData.typeData.singleType,
+                                pokeData.typeData.doubleType,
+                            ]}
+                        />
+                    </div>
+                    <div className='flex-2 bg-white rounded-lg'>
+                        <BarChart barData={pokeData.typeData.typesSum} />
+                    </div>
+                </div>
+                <SearchBar pokeData={pokeData.pokemonData} />
+            </div>
+
+            <PokeStats />
+        </div>
+    );
 };
 
 export default Dashboard;
