eb0fc8b18f38c9948bcad9e30e5a5aa4c27195ec
Ray Nimalan <raymondnimalan@gmail.com>
2024-09-15 20:15:59 -0400

    feat: add searchbar component base

---
+++ components/SearchBar.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+interface PokeCardProps {
+    pokeData: [];
+}
+
+interface Pokemon {}
+const SearchBar = ({ pokeData }: PokeCardProps) => {
+    return (
+        <div className='w-full flex flex-col w-[600px] items-center'>
+        </div>
+    );
+};
+
+export default SearchBar;
